ddbe32c2b5a54cf0ef90b8cfc7d546c20d725608
min-maxwell <z3464892@ad.unsw.edu.au>
2018-11-22 02:50:11 +1100

    started fiddling around

---
+++ set1/chal1/hextob64.c
@@ -0,0 +1,26 @@
+/*
+ * This program converts hex to base64 from the argument provided to the program at run-time
+ * Author: minmax
+ *
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char** argv, char** envp)
+{
+	// TODO
+}
+
+int b64_decode(char* ptr) 
+{
+	// TODO
+	// Decodes input to base64
+}
+
+int b64_encode(char* str)
+{
+	// TODO
+	// Encode input to base64
+}
